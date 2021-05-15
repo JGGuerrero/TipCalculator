d6e95ac1019775dd931ec71c4ed202130bc52fc0
unknown <jgguerrero@mail.fresnostate.edu>
2021-05-15 01:45:04 -0700

    logic complete

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * added lateinit var
+     */
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /**
+         * added
+         * binding = ActivityMainBinding.inflate(layoutInflater)
+         */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        /**
+         * replaced R.layout.activity_main with
+         * binding.root
+         */
+        setContentView(binding.root)
+
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null){
+            binding.tipResult.text = ""
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val selectedID = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedID) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
